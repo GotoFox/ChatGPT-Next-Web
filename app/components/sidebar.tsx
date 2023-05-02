e5b4967f50f755de6ca2d3615d1ab4cd385e6c79
GotoFox <gotogotofox@foxmail.com>
2023-05-02 12:14:27 +0800

    feat:style and title/introduce

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -96,9 +96,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>ChatGPT-Client</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Build Your Own Intelligent AI.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
