6c9cfc08622cc4fb84b9b9e9d6302604915ceca5
GotoFox <gotogotofox@foxmail.com>
2023-05-06 10:39:27 +0800

    feact:Access to Baidu Statistics

---
+++ public/baidustatistics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a04f6f152d329217203b33d8d8303d7e";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
