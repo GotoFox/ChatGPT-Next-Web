c46ac71b8e3e752e86f80897d309977f944e04d7
GotoFox <gotogotofox@foxmail.com>
2023-05-09 08:58:32 +0800

    fix:Deployment exception

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -22,8 +22,7 @@ import {
   useLocation,
 } from "react-router-dom";
 import { SideBar } from "./sidebar";
-import { useAppConfig } from "../store/config";
-import { useMaskStore } from "../store/mask";
+import { useAppConfig } from "@/app/store";
 
 export function Loading(props: { noLogo?: boolean }) {
   return (
