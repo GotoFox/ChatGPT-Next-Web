73dbd27f99f1b985c5cbec4fc6888bba67e30411
GotoFox <gotogotofox@foxmail.com>
2023-07-04 09:42:56 +0800

    feat:更新版本功能注释

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -356,7 +356,7 @@ export function Settings() {
             </Popover>
           </ListItem>
 
-          <ListItem
+          {/*<ListItem
             title={Locale.Settings.Update.Version(currentVersion ?? "unknown")}
             subTitle={
               checkingUpdate
@@ -379,7 +379,7 @@ export function Settings() {
                 onClick={() => checkUpdate(true)}
               />
             )}
-          </ListItem>
+          </ListItem>*/}
 
           <ListItem title={Locale.Settings.SendKey}>
             <Select
