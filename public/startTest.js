3b34cc95f961ecd7ebad42878c3de0dfb48e3abc
GotoFox <gotogotofox@foxmail.com>
2023-07-15 09:46:12 +0800

    update:允许调试

--- public/startTest.js
+++ public/startTest.js
@@ -1,39 +1,33 @@
 (function noDebuger() {
-  var _0x1a2712 =
-    new URL(
-      window["\u006c\u006f\u0063\u0061\u0074\u0069\u006f\u006e"][
-        "\u0068\u0072\u0065\u0066"
-      ],
-    )[
-      "\u0073\u0065\u0061\u0072\u0063\u0068\u0050\u0061\u0072\u0061\u006d\u0073"
-    ]["\u0067\u0065\u0074"]("gubed".split("").reverse().join("")) ===
-    "eslaf".split("").reverse().join("");
-  if (_0x1a2712) {
+  var isDebugEnabled =
+    new URL(window.location.href).searchParams.get("debug") === "false";
+  if (isDebugEnabled) {
     return;
   }
-  function _0x370673() {
-    var _0x4454f1 = new Date();
+
+  function testDebuger() {
+    var d = new Date();
     debugger;
-    if (new Date() - _0x4454f1 > (0x78193 ^ 0x78199)) {
-      document["\u0062\u006f\u0064\u0079"][
-        "\u0069\u006e\u006e\u0065\u0072\u0048\u0054\u004d\u004c"
-      ] = "".split("").reverse().join("");
-      return !![];
+    if (new Date() - d > 10) {
+      document.body.innerHTML = "";
+      return true;
     }
-    return ![];
+    return false;
   }
-  function _0xfd443d() {
-    while (_0x370673()) {
-      _0x370673();
+
+  function start() {
+    while (testDebuger()) {
+      testDebuger();
     }
   }
-  if (!_0x370673()) {
-    window["\u006f\u006e\u0062\u006c\u0075\u0072"] = function () {
+
+  if (!testDebuger()) {
+    window.onblur = function () {
       setTimeout(function () {
-        _0xfd443d();
-      }, 0x39c29 ^ 0x39ddd);
+        start();
+      }, 500);
     };
   } else {
-    _0xfd443d();
+    start();
   }
 })();
